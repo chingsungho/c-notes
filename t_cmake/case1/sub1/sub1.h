0cc2cf85b48ee0232042624f76e5130bf7918f38
Ching Sung <chingsung@excapp.top>
2024-09-03 00:02:14 +0700

    add cmake code

---
+++ t_cmake/case1/sub1/sub1.h
@@ -0,0 +1,15 @@
+
+#ifndef SUB1_H
+#define SUB1_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void sub1(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ t_cmake/case1/sub2/sub2.h
@@ -0,0 +1,15 @@
+
+#ifndef SUB2_H
+#define SUB2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void sub2(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
